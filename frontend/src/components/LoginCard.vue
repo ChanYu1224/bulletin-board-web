b5ebedc97ea387a3f08bcdff26d03e39b64489b0
ChanYu1224 <kinkitunekin@gmail.com>
2022-03-12 22:15:07 +0900

    fix indent

--- frontend/src/components/LoginCard.vue
+++ frontend/src/components/LoginCard.vue
@@ -6,31 +6,31 @@
     <v-card-text>
       <v-form v-model="isValid">
         <v-text-field
-          label="メールアドレス"
-          v-model="email"
-          type="email"
-          :rules="[rules.required, rules.isMailAddress]"
-          prepend-icon="mdi-email"
+        label="メールアドレス"
+        v-model="email"
+        type="email"
+        :rules="[rules.required, rules.isMailAddress]"
+        prepend-icon="mdi-email"
         ></v-text-field>
         <v-text-field
-          label="パスワード"
-          v-model="password"
-          :type="passwordVisible? 'text':'password'"
-          :rules="[rules.required, rules.lengthMoreThan6]"
-          @keypress.enter="login"
-          prepend-icon="mdi-lock"
-          :append-icon="passwordVisible? 'mdi-eye':'mdi-eye-off'"
-          @click:append="passwordVisible = !passwordVisible"
+        label="パスワード"
+        v-model="password"
+        :type="passwordVisible? 'text':'password'"
+        :rules="[rules.required, rules.lengthMoreThan6]"
+        @keypress.enter="login"
+        prepend-icon="mdi-lock"
+        :append-icon="passwordVisible? 'mdi-eye':'mdi-eye-off'"
+        @click:append="passwordVisible = !passwordVisible"
         ></v-text-field>
       </v-form>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn 
-        @click="login"
-        color="blue-grey darken-2"
-        class="blue-grey--text text--lighten-5"
-        :disabled="!isValid || loading"
+      @click="login"
+      color="blue-grey darken-2"
+      class="blue-grey--text text--lighten-5"
+      :disabled="!isValid || loading"
       >ログイン</v-btn>
     </v-card-actions>
   </v-card>
@@ -56,22 +56,24 @@ export default {
   },
   methods:{
     login(){
-      this.loading = true;
-      const authData = {
-        email: this.email,
-        password: this.password,
-      };
-      this.$store.dispatch('login', authData)
-      .then(() => {
-        if(!this.$store.getters.isValid){
-          sweetAlert.fire({
-            title: 'ログインできませんでした',
-            text: 'メールアドレスもしくはパスワード、又は両方が間違っています。',
-            showConfirmButton: true,
-          });
-        }
-        this.loading = false;
-      });
+      if(this.isValid){
+        this.loading = true;
+        const authData = {
+          email: this.email,
+          password: this.password,
+        };
+        this.$store.dispatch('login', authData)
+        .then(() => {
+          if(!this.$store.getters.isValid){
+            sweetAlert.fire({
+              title: 'ログインできませんでした',
+              text: 'メールアドレスもしくはパスワード、又は両方が間違っています。',
+              showConfirmButton: true,
+            });
+          }
+          this.loading = false;
+        });
+      }
     }
   },
 }
--- frontend/src/components/RegisterCard.vue
+++ frontend/src/components/RegisterCard.vue
@@ -6,50 +6,51 @@
     <v-card-text>
       <v-form v-model="isValid">
         <v-text-field
-          label="ユーザー名"
-          v-model="name"
-          :rules="[rules.required]"
-          prepend-icon="mdi-account-circle"
+        label="ユーザー名"
+        v-model="name"
+        :rules="[rules.required]"
+        prepend-icon="mdi-account-circle"
         ></v-text-field>
         <v-text-field
-          label="登録メールアドレス"
-          v-model="email"
-          type="email"
-          :rules="[rules.required, rules.isMailAddress]"
-          prepend-icon="mdi-email"
+        label="登録メールアドレス"
+        v-model="email"
+        type="email"
+        :rules="[rules.required, rules.isMailAddress]"
+        prepend-icon="mdi-email"
+        
         ></v-text-field>
         <v-text-field
-          label="パスワード"
-          v-model="password"
-          type="password"
-          :rules="[rules.required, rules.lengthMoreThan6]"
-          prepend-icon="mdi-lock"
+        label="パスワード"
+        v-model="password"
+        type="password"
+        :rules="[rules.required, rules.lengthMoreThan6]"
+        prepend-icon="mdi-lock"
         ></v-text-field>
         <v-text-field
-          label="パスワード（確認）"
-          v-model="passwordForCheck"
-          type="password"
-          :rules="[rules.required, rules.lengthMoreThan6, rules.isSamePassword]"
-          prepend-icon="mdi-lock"
+        label="パスワード（確認）"
+        v-model="passwordForCheck"
+        type="password"
+        :rules="[rules.required, rules.lengthMoreThan6, rules.isSamePassword]"
+        prepend-icon="mdi-lock"
         ></v-text-field>
         <v-textarea
-          label="プロフィール"
-          v-model="profile"
-          type="text"
-          :rules="[rules.lengthLessThan200]"
-          counter="200"
-          outlined
-          auto-grow
+        label="プロフィール"
+        v-model="profile"
+        type="text"
+        :rules="[rules.lengthLessThan200]"
+        counter="200"
+        outlined
+        auto-grow
         ></v-textarea>
       </v-form>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn 
-        @click="register"
-        color="blue-grey darken-2"
-        class="blue-grey--text text--lighten-5"
-        :disabled="!isValid || loading"
+      @click="register"
+      color="blue-grey darken-2"
+      class="blue-grey--text text--lighten-5"
+      :disabled="!isValid || loading"
       >会員登録</v-btn>
     </v-card-actions>
   </v-card>
@@ -99,7 +100,6 @@ export default {
       this.$store.dispatch('register', this.registerRequest)
       .then(() => {
         if(!this.$store.getters.isValid){
-          console.log('fire')
           sweetAlert.fire({
             title: '会員登録に失敗しました。',
             text: '正しく情報が入力されていないか、既に登録されているメールアドレスです。',
